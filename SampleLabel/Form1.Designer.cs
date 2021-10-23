32fcb7b895f70f53b611e31f03d92fde9cfce7fa
Siddharth Kumar Jain <siddjain@adobe.com>
2021-10-23 19:48:23 +0530

    Changing default report to A4_56 instead of A4_56_New

--- SampleLabel/Form1.Designer.cs
+++ SampleLabel/Form1.Designer.cs
@@ -173,8 +173,8 @@ namespace VTBarcode
             this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
-            "A4-56-New",
             "A4-56",
+            "A4-56-New",
             "A4-65"});
             this.comboBox1.Location = new System.Drawing.Point(328, 10);
             this.comboBox1.Name = "comboBox1";
